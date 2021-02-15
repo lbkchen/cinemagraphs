3ab66a31ab1b8e3d9adb72fff8c1eb27e142503a
Ken Chen <ken.chen@samsara.com>
2021-02-14 17:44:05 -0800

    Basic radial mask following cursor

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import waterfall from "./video_preview_h264.gif";
+
 import "./App.css";
 
 const App = () => {
+  const c = React.useRef(null); // Canvas context
+  const img = React.useRef(null); // Source image
+
   const handleVideoRef = React.useCallback((node) => {
     if (!node) {
       return;
     }
 
-    // Jank
+    // Jank: Width and height don't seem to get set immediately thus the delay
     setTimeout(() => {
+      img.current = node;
       const { width, height } = node;
       const containerNode = document.getElementById("canvas-container");
 
@@ -22,26 +27,49 @@ const App = () => {
       const canvasNode = document.getElementById("canvas");
 
       const ctx = canvasNode.getContext("2d");
-
-      // Compute first frame and draw onto canvas
-      ctx.drawImage(node, 0, 0, width, height);
-      const frame = ctx.getImageData(0, 0, width, height);
-      console.log(frame, frame.data);
-
-      // Apply alpha mask
-      // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
-      ctx.globalCompositeOperation = "destination-out";
-      const gradient = ctx.createLinearGradient(0, 0, 0, height);
-      gradient.addColorStop(0, "rgba(255, 255, 255, 0)");
-      gradient.addColorStop(1, "rgba(255, 255, 255, 1.0)");
-      ctx.fillStyle = gradient;
-      ctx.fillRect(0, 0, width, height);
+      c.current = ctx;
     }, 100);
   }, []);
 
+  const resetAlphaMask = (x, y, radiusInner = 50, radiusOuter = 100) => {
+    if (!c.current || !img.current) {
+      return;
+    }
+    const ctx = c.current;
+    const imgNode = img.current;
+    const { clientWidth: width, clientHeight: height } = ctx.canvas;
+
+    // Reset mask
+    ctx.clearRect(0, 0, width, height);
+    ctx.globalCompositeOperation = "source-over";
+
+    // Reapply static image
+    ctx.drawImage(imgNode, 0, 0, width, height);
+
+    // Reapply alpha mask
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
+    ctx.globalCompositeOperation = "destination-out";
+    const gradient = ctx.createRadialGradient(
+      x,
+      y,
+      radiusInner,
+      x,
+      y,
+      radiusOuter
+    );
+    gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+    gradient.addColorStop(1, "rgba(255, 255, 255, 0)");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, width, height);
+  };
+
+  const handleMouseMove = (e) => {
+    resetAlphaMask(e.clientX, e.clientY);
+  };
+
   return (
     <div id="App" className="App">
-      <div style={{ position: "relative" }}>
+      <div style={{ position: "relative" }} onMouseMove={handleMouseMove}>
         <div
           id="canvas-container"
           style={{ position: "absolute", top: 0, left: 0 }}
