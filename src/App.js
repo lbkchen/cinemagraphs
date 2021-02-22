87f09d57cea3b4fa2e7ab46c03153a153aedcebc
Ken Chen <ken.chen@samsara.com>
2021-02-21 21:15:03 -0800

    Use new method with three canvases to stop outside motion properly

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import tokyo from "./tokyo-0-10-stab-1080p.mp4";
 import "./App.css";
 
 const App = () => {
-  const c = React.useRef(null); // Canvas context
-  const img = React.useRef(null); // Source image
+  const c1 = React.useRef(null); // Top-level canvas that overlays on top of the video
+  const c2 = React.useRef(null); // Hidden canvas that caches the accumulated strokes
+  const c3 = React.useRef(null); // Hidden canvas that caches the inner region (Basically C2 - C1)
+  const img = React.useRef(null); // Source image/video node
 
   const handleVideoRef = React.useCallback((node) => {
     if (!node) {
@@ -19,25 +21,51 @@ const App = () => {
     setTimeout(() => {
       img.current = node;
       const { width, height } = node.getBoundingClientRect();
-      console.log(node, width, height, node.getBoundingClientRect());
       const containerNode = document.getElementById("canvas-container");
 
-      const canvas = (
-        <canvas id="canvas" width={width} height={height}></canvas>
+      const canvases = (
+        <>
+          <canvas
+            id="c3"
+            style={{ position: "absolute", top: 0, left: 0, zIndex: -2 }}
+            width={width}
+            height={height}
+          />
+          <canvas
+            id="c2"
+            style={{ position: "absolute", top: 0, left: 0, zIndex: -1 }}
+            width={width}
+            height={height}
+          />
+          {/* This one must sit above the video */}
+          <canvas
+            id="c1"
+            style={{ position: "absolute", top: 0, left: 0, zIndex: 1 }}
+            width={width}
+            height={height}
+          />
+        </>
       );
-      ReactDOM.render(canvas, containerNode);
-      const canvasNode = document.getElementById("canvas");
+      ReactDOM.render(canvases, containerNode);
 
-      const ctx = canvasNode.getContext("2d");
-      c.current = ctx;
+      const c1Node = document.getElementById("c1");
+      const c2Node = document.getElementById("c2");
+      const c3Node = document.getElementById("c3");
+
+      c1.current = c1Node.getContext("2d");
+      c2.current = c2Node.getContext("2d");
+      c3.current = c3Node.getContext("2d");
     }, 1000);
   }, []);
 
+  /**
+   * Old method that doesn't stop the rest of the frame from progressing
+   */
   const resetAlphaMask = (x, y, radiusInner = 50, radiusOuter = 100) => {
-    if (!c.current || !img.current) {
+    if (!c1.current || !c2.current || !c3.current || !img.current) {
       return;
     }
-    const ctx = c.current;
+    const ctx = c1.current;
     const imgNode = img.current;
     const { clientWidth: width, clientHeight: height } = ctx.canvas;
 
@@ -66,8 +94,84 @@ const App = () => {
     ctx.fillRect(0, 0, width, height);
   };
 
+  const resetC3 = ({ x, y, radiusInner = 50, radiusOuter = 100 }) => {
+    if (!c3.current || !img.current) {
+      return;
+    }
+    const ctx = c3.current;
+    const imgNode = img.current;
+    const { clientWidth: width, clientHeight: height } = ctx.canvas;
+
+    // Reset mask
+    ctx.clearRect(0, 0, width, height);
+    ctx.globalCompositeOperation = "source-over";
+
+    // Copy curr video frame
+    ctx.drawImage(imgNode, 0, 0, width, height);
+
+    // Mask with inner region
+    ctx.globalCompositeOperation = "destination-out";
+    const gradient = ctx.createRadialGradient(
+      x,
+      y,
+      radiusInner,
+      x,
+      y,
+      radiusOuter
+    );
+    gradient.addColorStop(0, "rgba(255, 255, 255, 0)");
+    gradient.addColorStop(1, "rgba(255, 255, 255, 1)");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, width, height);
+  };
+
+  const resetC2 = () => {
+    if (!c2.current || !c3.current) {
+      return;
+    }
+    const ctx = c2.current;
+    const { clientWidth: width, clientHeight: height } = ctx.canvas;
+
+    // Composite with current video in inner region: C2 = C2 + C3
+    ctx.globalCompositeOperation = "source-over";
+    ctx.drawImage(c3.current.canvas, 0, 0, width, height);
+  };
+
+  const resetC1 = ({ x, y, radiusInner = 50, radiusOuter = 100 }) => {
+    if (!c1.current || !c2.current) {
+      return;
+    }
+    const ctx = c1.current;
+    const { clientWidth: width, clientHeight: height } = ctx.canvas;
+
+    // Copy C2 without clearing
+    ctx.globalCompositeOperation = "source-over";
+    ctx.drawImage(c2.current.canvas, 0, 0, width, height);
+
+    // Mask with outer region
+    ctx.globalCompositeOperation = "destination-out";
+    const gradient = ctx.createRadialGradient(
+      x,
+      y,
+      radiusInner,
+      x,
+      y,
+      radiusOuter
+    );
+    gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+    gradient.addColorStop(1, "rgba(255, 255, 255, 0)");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, width, height);
+  };
+
   const handleMouseMove = (e) => {
-    resetAlphaMask(e.pageX, e.pageY, 300, 500);
+    const { pageX: x, pageY: y } = e;
+    const radiusInner = 200;
+    const radiusOuter = 400;
+
+    resetC3({ x, y, radiusInner, radiusOuter });
+    resetC2();
+    resetC1({ x, y, radiusInner, radiusOuter });
   };
 
   return (
@@ -77,7 +181,14 @@ const App = () => {
           id="canvas-container"
           style={{ position: "absolute", top: 0, left: 0 }}
         ></div>
-        <video ref={handleVideoRef} src={tokyo} autoPlay loop muted />
+        <video
+          ref={handleVideoRef}
+          src={tokyo}
+          style={{ position: "absolute", top: 0, left: 0, zIndex: 0 }}
+          autoPlay
+          loop
+          muted
+        />
       </div>
     </div>
   );
